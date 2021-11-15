43d18f6b476d6a7e518f35a12569b041cd5a5414
Thomas WEst <thom8750@stud.kea.dk>
2021-11-15 15:29:41 +0100

    15/11

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shell',
@@ -6,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shell.component.scss']
 })
 export class ShellComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+  .pipe(map(result => result.matches), 
+  shareReplay() 
+  );
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
